15935f7ce7aed2e2bbaffc80c0485f2fba1b546d
Igor Amidzic <amidzicigor46@gmail.co>
2020-02-07 21:28:14 -0500

    Fix path to dist for heroku

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './../client/web/dist')));
+app.use(express.static(path.join(__dirname, './../clients/web/dist')));
 
 const asyncHandler = (fn: any) => (req: Request, res: Response, next: any) => {
   return Promise
@@ -24,7 +24,7 @@ app.use('/api', require('./api/controllers/index'))
 
 // Return frontend app
 if (ENVIRONMENT === 'production') {
-  app.use('**', express.static(path.join(__dirname, './../client/web/dist')))
+  app.use('**', express.static(path.join(__dirname, './../clients/web/dist')))
 }
 
 export default app;
